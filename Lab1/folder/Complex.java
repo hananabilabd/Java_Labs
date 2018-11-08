8905f95be1240a5f9ce6d07f138e5172a086d51f
Hanna Nabil <hananabil@live.com>
2018-11-09 01:39:52 +0200

    Initial commit

---
+++ Lab1/folder/Complex.java
@@ -0,0 +1,88 @@
+//package Lab1;
+public class Complex{
+	int real=0 ;
+	int imag=0 ;
+	
+	public void setReal (int r){
+		real =r;
+	}
+	public void setImag (int i){
+		imag =i;
+	}
+	
+	public int getReal(){
+		return real;
+	}
+	public int getImag (){
+		return imag;
+	}
+	public void print(){
+		if (imag >0){
+		System.out.print(real);
+		System.out.print("+");
+		System.out.print(imag);
+		System.out.println("i");
+		}
+		else{
+			System.out.print(real);
+			System.out.print(imag);
+			System.out.println("i");
+		}
+	}
+	public void setComplex (int r , int i){
+		real=r;
+		imag=i;
+	}
+	public void setComplex (int v){
+		imag=real=v;
+	}
+	
+	public static Complex add(Complex a,Complex b){
+		Complex c = new Complex();
+		c.setReal(a.getReal()+b.getReal());
+		c.setImag(a.getImag()+b.getImag());
+		return c;
+	}
+	public static Complex subtract(Complex a,Complex b){
+		Complex c = new Complex();
+		c.setReal(a.getReal()-b.getReal());
+		c.setImag(a.getImag()-b.getImag());
+		return c;
+	}
+	
+   
+	public static void main(String[] args){
+		int[] intArgs = new int[args.length];
+		for (int i=0 ;i<args.length; i++) 
+		{
+         try {
+            intArgs[i] = Integer.parseInt(args[i]);
+			System.out.println(intArgs[i]);
+         } catch (NumberFormatException e) {
+            System.err.println("Failed trying to parse a non-numeric argument, " + args[i]);
+         }
+         }
+		Complex c1 =new Complex();
+		Complex c2 = new Complex();
+		Complex c3 = new Complex();
+		 
+		Complex c4 = new Complex();
+		
+		c1.setComplex(3,4);
+		c2.setComplex(5,6);
+		System.out.print("c1=");
+		c1.print();
+		System.out.print("c2=");
+		c2.print();
+		c3=Complex.add(c1,c2);
+		System.out.print("c3=c1+c2=");
+		c3.print();
+		for (int i : intArgs) 
+		{
+         System.out.println(intArgs[i]);
+         }
+		
+		
+	}
+	
+}
---
+++ Lab1/hello.java
@@ -0,0 +1,12 @@
+package Lab1;
+class HelloWorld{
+	public static void main(String[] args){
+		
+		for(int i=0;i <args.length;i++){ 
+		System.out.print("Hello ");
+        System.out.print(args[i]);
+        System.out.println();		
+    } 
+		
+	}
+}
